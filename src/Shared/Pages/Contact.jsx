cdd7ac979700f051117db8e09b35b1863f0ca968
rimakhatun3 <rimamnt46@gmail.com>
2023-07-03 19:40:22 +0600

    udate my resume

--- src/Shared/Pages/Contact.jsx
+++ src/Shared/Pages/Contact.jsx
@@ -21,15 +21,15 @@ const Contact = () => {
             <form  ref={form} onSubmit={sendEmail} className='w-full mx-auto space-y-3   px-8 py-8' >
                <div className='w-full lg:w-2/4 mx-auto'>
                 
-               <input className=' w-full text-stone-700 px-4 border-orange-600  border-4 rounded-xl py-2' placeholder='Enter Your Name' type="text"  name="user_name" id="" />
+               <input className=' w-full text-stone-700 px-4 border-orange-600  border-4 rounded-xl py-2' placeholder='Name' type="text"  name="user_name" id="" />
                </div>
                <div className='w-full lg:w-2/4 mx-auto'>
                
-               <input className='border-orange-600  border-4 w-full px-4 text-stone-700 rounded-xl py-2' placeholder='Enter Your Email' type="text"  name="user_email" id="" />
+               <input className='border-orange-600  border-4 w-full px-4 text-stone-700 rounded-xl py-2' placeholder='Email' type="text"  name="user_email" id="" />
                </div>
                <div className='w-full lg:w-2/4 mx-auto'>
                 
-               <textarea placeholder='Enter Your ' className='w-full border-orange-600  border-4 text-stone-700 px-5 py-3 rounded-xl' name="message" id="" cols="15" rows="5"></textarea>
+               <textarea placeholder='Message ' className='w-full border-orange-600  border-4 text-stone-700 px-5 py-3 rounded-xl' name="message" id="" cols="15" rows="5"></textarea>
                </div>
     <div className='lg:w-1/4 w-full mx-auto my-4'>
     <input className='btn hover:text-black w-full bg-gradient-to-r from-[#f5af19] to-[#f12711] text-white' type="submit" value="Send Message" />
